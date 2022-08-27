77fe948af378f6641d2ad8a615bb1f3abb527696
andre_gthv1mg <anrei.kinder.170@gmail.com>
2022-08-27 20:12:53 +0500

    remember_method

--- redis_c/routes/web.php
+++ redis_c/routes/web.php
@@ -17,61 +17,25 @@ use Illuminate\Support\Facades\Redis;
 |
 */
 
+//function remember($key, $minutes, $callback){
+//    if ($value = Redis::get($key)){
+//        return json_decode($value);
+//    }
+//
+//
+//    Redis::setex($key, $minutes,  $value = $callback());
+//
+//
+//    return  $value;
+//}
+
 Route::get('/', function () {
 
-    /*
-    $userStat3 = [
-        'favourites' => 10,
-        'watchLaters' => 5,
-        'completions' => 6,
-    ];
-    Redis::hmset('user.1.stats', $userStat3); */
+    return Cache::remember('articles.all', 60*60, function (){
+        return \App\Models\Article::all();
+    });
+//    return remember('articles.all', 60 * 60, function (){
+//        return \App\Models\Article::all();
+//    });
 
-//    \Illuminate\Support\Facades\Cache::put('foo2', 'bar2', 10);
-//
-//    return \Illuminate\Support\Facades\Cache::get('foo2');
-
-
-
-    //return Cache::store('redis');
-    Cache::put('bar3', 'baz32');
-    $val = Cache::get('bar3');
-
-
-    return $val;
-
-    //return Redis::hgetall('user.1.stats');
-});
-
-Route::get('/user/{id}/stats', function ($id) {
-    return Redis::hgetall("user.{$id}.stats");
-});
-
-
-Route::get('favourite-video', function () {
-    Redis::hincrby('user.1.stats', 'favourites', 1);
-    return redirect('/');
-});
-
-
-Route::get('articles/trending', function () {
-
-    $trending = Redis::zrevrange('trending_articles', 0, 2);
-
-    $trending = \App\Models\Article::hydrate(
-      array_map('json_decode', $trending)
-    );
-
-    //\App\Models\Article::hydrateFromJsonString($trending );
-
-    return $trending;
-});
-
-
-
-Route::get('articles/{article}', function (\App\Models\Article $article) {
-    Redis::zincrby('trending_articles', 1 , $article->toJson());
-
-    //Redis::zremrangebyrank('trending_articles', 0, -4);
-    return $article;
 });
